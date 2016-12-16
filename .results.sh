ba6343bbd158e0a10a78265717ceda83545425b0
TozerCL21 <tozerc@cppasd.com>
2016-12-16 10:53:05 -0500

    Fix file errors

--- .results.sh
+++ .results.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while [ test -s /usr/share/smend/data/result.txt || true ]; do
+while [ test -s /usr/share/smend/data/results.txt || true ]; do
 notify-send -u critical "Insufficent permissions, or file cannot be found."
 else 
 echo "All clear"
